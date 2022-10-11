5707cd36d128cc69cb14bd9fede018284a4831dd
Saran871 <sarankumars63538@gmail.com>
2022-10-11 22:20:13 +0530

    update changes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,6 +10,8 @@ import {useForm} from 'react-hook-form'
 import { Form, Container, Col, Row, InputGroup } from 'react-bootstrap'
 import instance from "../service/service";
 import { isLogin } from "./isLogin";
+import  { motion } from 'framer-motion'
+import { margin } from "@mui/system";
 
 const schema = yup.object({
   email: yup.string().email('must be valid e-mail address').required('E-Mail is required'),
@@ -33,60 +35,64 @@ if(isLogin())navigate('/home');
       password:data.password
     }
     
-    instance.post(process.env.REACT_APP_LOGIN,login_data).then( res =>{ 
-      console.log(res.data.responseResult);
-      const _data = JSON.stringify( res.data.responseResult)
-      localStorage.setItem('data',_data)
-      // instance.defaults.headers.common['Authorization']=`Bearer ${res.data.token}`
-      localStorage.setItem('token', res.data.responseResult.token)
+    // instance.post(process.env.REACT_APP_LOGIN,login_data).then( res =>{ 
+    //   console.log(res.data.responseResult);
+    //   const _data = JSON.stringify( res.data.responseResult)
+    //   localStorage.setItem('data',_data)
+    //   // instance.defaults.headers.common['Authorization']=`Bearer ${res.data.token}`
+    //   localStorage.setItem('token', res.data.responseResult.token)
 
-      navigate('/home')      
-    }).catch( err => {
-      console.log(err.message)
-    })    
+    //   navigate('/home')      
+    // }).catch( err => {
+    //   console.log(err.message)
+    // })    
     
   }   
-  return (    
-    <Container>
-      {/* {(isLogin())&&navigate('/home')} */}
-      <Row className="my-5">
-        <Col  md={8} sm={12} lg className="align-items-center text-center my-5 py-5">
-          <div className="w-auto h-auto shadow-lg">
-            <img src={loginImg} className="w-100 h-100" alt="Employee image"/>
-          </div>
-        </Col>
-        <Col sm={12} md={5} lg className="align-items-center text-center my-5 py-5">
-          <Form className="d-flex flex-column shadow-lg py-5 px-3 rounded-4 " onSubmit={handleSubmit(onSubmit)}>
-            <div className="swing py-2 shadow-md m-auto rounded-4 mt-5 mb-5">
-              <img src={cgsimg} alt="cgs image" className="fs-3 px-2 py-1" />
+  return (   
+    <motion.div initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} >
+      <Container>
+        < Row className="my-5">
+          {/* <Col  md={8} sm={12} lg className="align-items-center text-center my-5 py-5">
+            <div className="w-auto h-auto shadow-lg">
+              <img src={loginImg} className="w-100 h-100" alt="Employee image"/>
             </div>
-            <div className=" text-center m-auto mb-5">
-               <InputGroup>
-                <InputGroup.Text><Ai.AiOutlineMail /></InputGroup.Text> 
-                <Form.Control 
-                  {...register('email')}
-                  placeholder="E-Mail Address" 
-                  className="form-control fs-6" 
-                /> 
-                <p className="text-danger mt-2">{errors.email?.message}</p>
-              </InputGroup>
-            </div>
-            <div className=" text-center m-auto mb-5">
-              <InputGroup>
-                  <InputGroup.Text><Ai.AiOutlineLock /></InputGroup.Text> 
-                <Form.Control 
-                {...register('password')} type="password"
-                  className="form-control" placeholder="Password"/>   
-                <p className="text-danger mt-2">{errors.password?.message}</p>   
-              </InputGroup>         
-            </div>
-            <div className="text-center m-auto mb-5 mt-5 btn-lg">
-              <input type="submit" value="Submit" className="btn btn-primary btn-lg mx-3 fs-6 rounded-4"/> 
-            </div>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          </Col> */}
+        <motion.div animate={{opacity:1,justifyContent:'center',alignItems:'center'}} transition={{duration:0.5}}>
+          <Col sm={12} md={6} lg className="m-2 p-2">
+            <Form className="d-flex flex-column shadow-lg py-3 px-3 rounded-4 " onSubmit={handleSubmit(onSubmit)}>
+              <motion.div transition={{delay:0.3, duration:0.3}} initial={{x:'-100vw'}} animate={{x:0}} className="swing py-2 shadow-md m-auto rounded-4 mt-5 mb-5">
+                <img src={cgsimg} alt="cgs image" className="fs-3 px-2 py-1" />
+              </motion.div>
+              <div className=" text-center m-auto mb-5">
+                <InputGroup>
+                  <InputGroup.Text><Ai.AiOutlineMail /></InputGroup.Text> 
+                  <Form.Control 
+                    {...register('email')}
+                    placeholder="E-Mail Address" 
+                    className="form-control fs-6" 
+                  /> 
+                  <p className="text-danger mt-2">{errors.email?.message}</p>
+                </InputGroup>
+              </div>
+              <div className=" text-center m-auto mb-5">
+                <InputGroup>
+                    <InputGroup.Text><Ai.AiOutlineLock /></InputGroup.Text> 
+                  <Form.Control 
+                  {...register('password')} type="password"
+                    className="form-control" placeholder="Password"/>   
+                  <p className="text-danger mt-2">{errors.password?.message}</p>   
+                </InputGroup>         
+              </div>
+              <div className="text-center m-auto mb-5 mt-5 btn-lg">
+                <input type="submit" value="Submit" className="btn btn-primary btn-lg mx-3 fs-6 rounded-4"/> 
+              </div>
+            </Form>
+          </Col>
+          </motion.div>
+        </Row>
+      </Container>
+    </motion.div> 
+    
   );
 };
 
